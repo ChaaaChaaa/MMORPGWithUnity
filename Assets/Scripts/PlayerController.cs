2d96d041f78b29bd4ee66874560d134be313ba3f
MJ Choi <1621139@gmail.com>
2021-04-26 00:03:40 +0900

    [UPDATE] Rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,36 +15,57 @@ public class PlayerController : MonoBehaviour
      *   - PlayerController (*)
      */
 
+    float _yAngle = 0.0f;
+
     void Update() //1frame당 호출 
     {
-        //  Time.deltaTime 지금 frame - 이전 frame 로 구함 : 너무 빠른것을 방지
-        // transform 조정 상하좌우
+        // _yAngle += Time.deltaTime * 100.0f;
+
+        // 1. 값을 그냥 넣는 방식 : 절대 회전값 지정하여 회전
+        // transform.eulerAngles = new Vector3(0.0f, _yAngle, 0.0f);
+        // =  transform.rotation = Quaternion.Euler(new Vector3(0.0f, _yAngle, 0.0f));
+
+        //2. 회전하는 값을 각 축에 넣어주는 방식(+= delta) : 특정 축을 기준으로 얼마만큼 회전
+        //transform.Rotate(new Vector3(0.0f, Time.deltaTime * 100.0f, 0.0f));
+
+
+
         if (Input.GetKey(KeyCode.W))
-        { 
-            // transform.position += new Vector3(0.0f, 0.0f, 1.0f) * Time.deltaTime * _speed; //거리 속도 시간, 속도가 1 frame 당이여서 너무 빠름
-            //transform.position += transform.TransformDirection(Vector3.forward * Time.deltaTime * _speed); // global 좌표 += 글로벌변환기(로컬 좌표)
-            transform.Translate(Vector3.forward * Time.deltaTime * _speed); //자기가 바라보는 로컬로 계산하므로 로컬좌표를 넣어도 가능 
+        {
+            //transform.rotation = Quaternion.LookRotation(Vector3.forward); //Word 기준으로 특정 방향을 바라봄
+            //좀 더 부드럽게 처리 Slerp(Quaternion a,Quaternion b,float t)
+            //                             : a:현재 위치, b: 목적지 0~1 :a에 가까움~b에 가까움
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
+
+            //이동이 부자연스러움
+            // -> Slerp로 0.2f시 2시방향 정도를 쳐다보능 상황에서 앞으로 가기때문에 똑바로 앞으로 가지 않고 조금 옆으로 이동
+            //transform.Translate(Vector3.forward * Time.deltaTime * _speed); //회전 후 앞을 바라보므로 모두 앞 방향으로 설정
+
+            //rotation과 함께 이동함수를 쓸 경우에는 이런 부분을 맞춰줘야함
+            //바라보는 방향과 상관없이 word 좌표로 앞을 보니까 아래와 같이 변경함 -> 바라보는 방향이 관련 없어서 실제 바라볼 뱡향으로 변경
+            transform.position += Vector3.forward * Time.deltaTime * _speed;
+
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            //transform.position -= new Vector3(0.0f, 0.0f, 1.0f) * Time.deltaTime * _speed;
-            //transform.position += transform.TransformDirection(Vector3.back * Time.deltaTime * _speed);
-            transform.Translate(Vector3.back * Time.deltaTime * _speed);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
+            //transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+            transform.position += Vector3.back * Time.deltaTime * _speed;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            //transform.position -= new Vector3(1.0f, 0.0f, 0.0f) * Time.deltaTime * _speed;
-            //transform.position += transform.TransformDirection(Vector3.left * Time.deltaTime * _speed);
-            transform.Translate(Vector3.left * Time.deltaTime * _speed);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
+            //transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+            transform.position += Vector3.left * Time.deltaTime * _speed;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            //transform.position += new Vector3(1.0f, 0.0f, 0.0f) * Time.deltaTime * _speed; ;
-            //transform.position += transform.TransformDirection(Vector3.right * Time.deltaTime * _speed);
-            transform.Translate(Vector3.right * Time.deltaTime * _speed);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
+            //transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+            transform.position += Vector3.right * Time.deltaTime * _speed;
         }
     }
 }
